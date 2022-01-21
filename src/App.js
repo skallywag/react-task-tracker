8f897797839790d729e174bf68417e3a617362e0
Samuel Lee <lee.sam1492@gmail.com>
2022-01-20 17:22:53 -0700

    react-app-intro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from 'react'
+import AddTask from './components/AddTask';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
 {
     id: 1,
@@ -19,13 +21,31 @@ const App = () => {
     text: 'Food shopping',
     reminder: false
 }])
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    // console.log(id);
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
 
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
+  //Toggle Reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
 
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Tasks Complete'}
     </div> 
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import Button from "./Button";
 
-const Header = ( {title} ) => {
-
-    const onClick = () => {
-        console.log('click');
-    }
-    
+const Header = ( {title, onAdd, showAdd} ) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'close' : 'Add'} onClick={onAdd}/>
     </header>
   )
 };
